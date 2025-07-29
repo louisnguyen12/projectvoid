3632ebea6aaa08173e707e8c9755bb2b279c9ef3
louisnguyen12 <qnguyen1209@gmail.com>
2025-07-29 09:53:08 +0700

    First Commit: - Added Player - Added Git Plugin version control - Added Level - Added Enemies - Added Tileset

---
+++ player/player.gd
@@ -0,0 +1,70 @@
+extends CharacterBody2D
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+const GRAVITY = 1000
+@export var speed : int = 300
+@export var jump : int = -300
+@export var jump_horizontal : int = 100
+
+enum State { Idle, Run, Jump }
+
+var current_state : State
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	current_state = State.Idle
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	player_falling(delta)
+	player_idle(delta)
+	player_run(delta)
+	player_jump(delta)
+	move_and_slide()
+	
+	player_animation()
+	print("State: ", State.keys()[current_state])
+	
+func player_falling(delta: float):
+	if !is_on_floor():
+		velocity.y += GRAVITY * delta
+
+func player_idle(delta: float):
+	if is_on_floor():
+		current_state = State.Idle
+	
+func player_run(delta: float):
+	if !is_on_floor():
+		return
+	var direction = input_movement()
+	
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+	if direction != 0:
+		current_state = State.Run
+		animated_sprite_2d.flip_h = false if direction > 0 else true
+		
+func player_jump(delta: float):
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = jump
+		current_state = State.Jump
+		
+	if !is_on_floor() and current_state == State.Jump:
+		var direction = input_movement()
+		velocity.x += direction * jump_horizontal * delta
+
+func player_animation():
+	if current_state == State.Idle:
+		animated_sprite_2d.play("idle")
+	elif current_state == State.Run:
+		animated_sprite_2d.play("run")
+	elif current_state == State.Jump:
+		animated_sprite_2d.play("jump")
+
+func input_movement():
+	var direction : float = Input.get_axis("move_left", "move_right")
+	return direction
