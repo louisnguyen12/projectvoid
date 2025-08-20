c31da5f1f19b31233d824043ab73d41000fd2b30
louisnguyen12 <qnguyen1209@gmail.com>
2025-08-20 11:04:18 +0700

    - Added level design for Level 1 - Added NPCs houses - Added metal bars - Need to create another area for NPC houses - The lever to open the metal bar should be inside the NPC house above the platform.

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,7 +3,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	RenderingServer.set_default_clear_color(Color(0.45,0.43,0.16,1.00))
+	RenderingServer.set_default_clear_color(Color(0.18,0.74,1.00,1.00))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
